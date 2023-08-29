7c6cba3727ba9a99576dcf4f49b93ac30ce6716c
s-hanaka <shimekake.hanaka@gmail.com>
2023-08-29 19:00:14 +0900

    コメント変更

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\TodoRequest;
 use App\Todo;
-// use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
@@ -14,14 +13,12 @@ class TodoController extends Controller
     public function __construct(Todo $todo)
     {
         $this->todo = $todo;
-        // dd($todo); //オブジェクト型が入っている
     }
 
     // Todo一覧表示（top画面）
     public function index()
     {
         $todos = $this->todo->all();
-        // dd($todos); //配列型が入っている
         return view('todo.index', ['todos' => $todos]);
     }
 
@@ -34,12 +31,8 @@ class TodoController extends Controller
     // データ登録（新規作成）
     public function store(TodoRequest $request)
     {
-        // dd($request); //オブジェクト型が入っている。
         $inputs = $request->all();
-        // dd($inputs); //配列型が入っている。httpから受け取ったすべてのフォームデータ。トークンとコンテンツ。
-        // ↓Eloquentモデルのfillメソッドでsql文セット
         $this->todo->fill($inputs);
-        // insert into todos (content,  ,  ) values ('あああ',  ,  );
         $this->todo->save();
         return redirect()->route('todo.index');
     }
@@ -47,10 +40,7 @@ class TodoController extends Controller
     // 参照（編集前後の詳細画面用）
     public function show($id)
     {
-        // dd($id); //整数
         $todo = $this->todo->find($id);
-        // dd($todo); // findはIDカラムで探す
-        // select * from todos where id = $id
         return view('todo.show', ['todo' => $todo]);
     }
 
@@ -58,7 +48,6 @@ class TodoController extends Controller
     public function edit($id)
     {
         $todo = $this->todo->find($id);
-        // select * from todos where id = $id
         return view('todo.edit', ['todo' => $todo]);
     }
 
@@ -67,11 +56,8 @@ class TodoController extends Controller
     {
         $inputs = $request->all();
         $todo = $this->todo->find($id);
-        // select * from todos where id = $id; idを指定
         $todo->fill($inputs);
-        // updateしたいカラムとレコードの指定　update todos set $input(カラム名Key = value) where id = $id;
         $todo->save();
-        // dd($todo); // findはIDカラムで探す
         return redirect()->route('todo.show', $todo->id);
     }
     // delete
--- src/app/Todo.php
+++ src/app/Todo.php
@@ -5,14 +5,12 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-// Model クラスを継承している。Eloquentの機能を持つモデル
 class Todo extends Model
 {
     // traitを使うためにclassに組み込み
     // softdaletトレイトは、論理削除するためのトレイト。
     use SoftDeletes;
 
-    // protectedは自身のクラスと継承されたクラスで使用可能
     protected $table = 'todos';
 
     protected $fillable = [
